4a11e1b53d5d05c85df1b9aa97e908e417513945
hausofanna <annacanivell97@gmail.com>
2024-06-25 16:33:54 +0200

    subida

---
+++ src/test/java/com/taimBack/TaimBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taimBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaimBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
