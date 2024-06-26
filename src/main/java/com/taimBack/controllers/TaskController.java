276d423d3aa96447395249e45ea60a559f7f4a6e
hausofanna <annacanivell97@gmail.com>
2024-06-26 15:50:09 +0200

    task correción

--- src/main/java/com/taimBack/controllers/TaskController.java
+++ src/main/java/com/taimBack/controllers/TaskController.java
@@ -1,11 +1,14 @@
 package com.taimBack.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,8 +37,14 @@ public class TaskController {
 	}
 	
 	@GetMapping("/")
+	public List<Task> selectTask(){
+		return taskRepository.findAll();
+	}
+	
+	@PutMapping("/{id}")
 	public void updateTask(@RequestBody Task task, @PathVariable("id") Integer id) {
 		task.setTaskId(id);
 		taskRepository.save(task);
 	}
+	
 }
