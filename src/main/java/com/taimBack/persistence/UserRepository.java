a843958655703d364bfcba75a7ba4448f78c427e
dashiys <dariadariays2@gmail.com>
2024-06-26 15:48:39 +0200

    user entity, controller y repository

---
+++ src/main/java/com/taimBack/persistence/UserRepository.java
@@ -0,0 +1,13 @@
+package com.taimBack.persistence;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.taimBack.entities.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+	List<User> findAll();
+}
+
