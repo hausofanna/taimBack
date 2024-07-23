238e8663b0d9625f5e1972102888bbc3ee74ec8a
hausofanna <annacanivell97@gmail.com>
2024-07-23 16:36:15 +0200

    profile pic

--- src/main/java/com/taimBack/controllers/UserController.java
+++ src/main/java/com/taimBack/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.taimBack.controllers;
 
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +18,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.taimBack.config.UserService;
 import com.taimBack.entities.User;
@@ -24,58 +31,74 @@ import com.taimBack.persistence.UserRepository;
 @RestController // Para hacer peticiones REST
 @RequestMapping("/user")
 public class UserController {
-	@Autowired
-	private UserRepository userRepository;
 
-//	@Autowired
-//	BCryptPasswordEncoder encoder;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@GetMapping("/")
-	public List<UserDTO> getAllUsers() {
-		List<User> users = userRepository.findAll();
-		return users.stream().map(userService::toUserDTO).collect(Collectors.toList());
-	}
+    // Para el profile pic
+    private final Path root = Paths.get("uploads");
 
-	@GetMapping("/{id}")
-	public UserDTO getUserById(@PathVariable Integer id) {
-		Optional<User> user = userRepository.findById(id);
-		return user.map(userService::toUserDTO).orElse(null);
-	}
+    @GetMapping("/")
+    public List<UserDTO> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map(userService::toUserDTO).collect(Collectors.toList());
+    }
 
-	@PostMapping("/")
-	public UserDTO createUser(@RequestBody UserDTO userDTO) {
-		User user = userService.toUser(userDTO);
-		user = userRepository.save(user);
-		return userService.toUserDTO(user);
-	}
+    @GetMapping("/{id}")
+    public UserDTO getUserById(@PathVariable Integer id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.map(userService::toUserDTO).orElse(null);
+    }
 
-	@PutMapping("/{id}")
-	public UserDTO updateUser(@PathVariable Integer id, @RequestBody UserDTO userDTO) {
-		Optional<User> userOptional = userRepository.findById(id);
-		if (userOptional.isPresent()) {
-			User user = userOptional.get();
-			user.setUsername(userDTO.getUsername());
-			user.setName(userDTO.getName());
-			user.setSurname(userDTO.getSurname());
-			user.setEmail(userDTO.getEmail());
-			user.setLocation(userDTO.getLocation());
-			user.setDescription(userDTO.getDescription());
-			user.setSkills(userDTO.getSkills());
-			user.setInterests(userDTO.getInterests());
-			user.setRating(userDTO.getRating());
-			user.setProfilePic(userDTO.getProfilePic());
-			user = userRepository.save(user);
-			return userService.toUserDTO(user);
-		} else {
-			return null;
-		}
-	}
+    @PostMapping("/")
+    public UserDTO createUser(@RequestBody UserDTO userDTO) {
+        User user = userService.toUser(userDTO);
+        user = userRepository.save(user);
+        return userService.toUserDTO(user);
+    }
 
-	@DeleteMapping("/{id}")
-	public void deleteUser(@PathVariable Integer id) {
-		userRepository.deleteById(id);
-	}
+    @PutMapping("/{id}")
+    public UserDTO updateUser(@PathVariable Integer id, @RequestBody UserDTO userDTO) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setUsername(userDTO.getUsername());
+            user.setName(userDTO.getName());
+            user.setSurname(userDTO.getSurname());
+            user.setEmail(userDTO.getEmail());
+            user.setLocation(userDTO.getLocation());
+            user.setDescription(userDTO.getDescription());
+            user.setSkills(userDTO.getSkills());
+            user.setInterests(userDTO.getInterests());
+            user.setRating(userDTO.getRating());
+            user.setProfilePic(userDTO.getProfilePic());
+            user = userRepository.save(user);
+            return userService.toUserDTO(user);
+        } else {
+            return null;
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        userRepository.deleteById(id);
+    }
+    
+    // Para el profile pic
+    @PostMapping("/upload")
+    public ResponseEntity<?> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        try {
+            Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
+            return ResponseEntity.ok().body("{\"resp\":\"Archivo cargado con éxito\"}");
+        } catch (Exception e) {
+            if (e instanceof FileAlreadyExistsException) {
+                throw new RuntimeException("A file of that name already exists.");
+            }
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
+
